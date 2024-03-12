a70b98fe029221ba9f07f74e4500b58c4c195438
connorethan <connorethan@g.ucla.edu>
2024-03-12 16:22:53 -0700

    Update controller.v

--- final_project/final_project.srcs/sources_1/new/controller.v
+++ final_project/final_project.srcs/sources_1/new/controller.v
@@ -1,66 +1,77 @@
 `timescale 1ns / 1ps
-//    NES Controller SIPO    //
-// Developed by Michael Swan //
 
 module nes_controller(
-		input master_clock,
-        input serial_data,
-		// Device connections
-		output data_latch,
-        output data_clock,
-		// Data output
-		output reg [7:0] button_state,
-		output update_clock
-   );
-	// Unit Parameters //
-	parameter Hz  = 1;
-	parameter KHz = 1000*Hz;
-	parameter MHz = 1000*KHz;
-	
-	// Context-sensitive Parameters //
-	parameter MASTER_CLOCK_FREQUENCY = 100*MHz; // USER VARIABLE
-	parameter OUTPUT_UPDATE_FREQUENCY = 120*Hz; // USER VARIABLE
-	
-	// Clock divider register size
-	parameter DIVIDER_EXPONENT = log2( (MASTER_CLOCK_FREQUENCY / OUTPUT_UPDATE_FREQUENCY) / 10 ) - 2;
-	
-	// Generate a clock for generating the data clock and sampling the controller's output
-	reg [DIVIDER_EXPONENT:0] sample_count;
-	wire sample_clock = sample_count[DIVIDER_EXPONENT];
-	always @(posedge master_clock) sample_count <= sample_count + 1;
-	
-	// Keep track of the stage of the cycle
-	reg [3:0] cycle_stage;
-	reg [7:0] data;
-	// Generate control signals for the three phases
-	wire latch_phase = cycle_stage == 0;
-	wire data_phase = cycle_stage >= 1 & cycle_stage <= 8;
-	wire end_phase = cycle_stage == 9;
-	
-	// Handle inputs from the controller
-	always @(posedge sample_clock) begin
-		     if(latch_phase) data <= 4'h0;
-		else if(data_phase)  data <= {data[6:0], serial_data};
-		else if(end_phase) begin
-			cycle_stage <= 4'h0;
-			button_state[7:0] <= data;
-		end
-		cycle_stage <= cycle_stage + 1;
-	end
+    input wire clk, 
+    input wire data,                                        // input data from nes controller to FPGA
+    output reg latch = 0, nes_clk = 0,                              // outputs from FPGA to nes controller
+    output reg [7:0] abssudlr = 8'b0
+);
 
-	// Generate output signals
-	assign data_latch = latch_phase;
-	assign data_clock = data_phase & sample_clock;
-	assign update_clock = sample_clock;
-	
-	// Helper functions
-	function integer log2;
-		 input [31:0] value;
-		 begin
-			  value = value - 1;
-			  for (log2 = 0; value > 0; log2 = log2 + 1) begin
-					value = value >> 1;
-			  end
-		 end
-	endfunction
+    wire clockEnable;
+    clock60hz clk60hz(clk, clockEnable);
+    // states
+    localparam [3:0] 
+             latch_en = 4'h0,  
+             read_data = 4'h1,
+             null_state = 4'h2;
+    localparam
+            clk12us = 1200,
+            clk6us = clk12us / 2;  
+             
+    reg [10:0] count = 0;
+    
+    reg [3:0] state = null_state; 
+    reg [7:0] button_index = 8'd0;
+            
+ 
+    always @(posedge clk) begin
+        if (clockEnable && (state == null_state)) begin
+            state = latch_en;
+            count = 0;
+        end
+        case(state)
+        
+            latch_en: begin
+                if(count >= clk12us) begin
+                    latch = 0;
+                    if (count >= clk12us + clk6us) begin
+                        count = 0;
+                        button_index = 8'd1;
+                        state = read_data;
+                    end else count = count + 1;
+                end else begin
+                    latch = 1;
+                    count = count + 1;
+                end
+            end
+        
+            read_data: begin
+                if(count <= clk6us) begin
+                    nes_clk = 1; 
+                end else
+                    nes_clk = 0;
+                
+                if(count < clk12us) begin    
+                    if(count == clk6us)
+                        abssudlr[button_index] = ~data;
+                    count = count + 1;
+                end else if(count >= clk12us) begin
+                    button_index = button_index + 8'd1;
+                    
+                    if(button_index == 8) begin
+                        state = null_state;
+                        button_index = 0;
+                    end
+                    count = 0; 
+                end
+             
+            end
+            
+            default: begin
+                nes_clk = 0;
+                latch = 0;
+            end
+        endcase
+    end
+    
 endmodule
